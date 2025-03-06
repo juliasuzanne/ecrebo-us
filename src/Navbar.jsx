6d53b327c77ba2c51d27c858470fd5a5c9ec50cb
juliasuzanne <julia.s.grimes@gmail.com>
2025-03-06 10:32:05 -0500

    Remove p2pi from navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -49,11 +49,6 @@ export function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="nav-fill navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#p2pi">
-                P2PI
-              </a>
-            </li>
             <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle"
